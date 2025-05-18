abd6e3e483c4182eb15377a6aeba101a57014b7e
Arrabito <manuel.arra@outlook.com>
2025-05-18 17:10:26 +0200

    Commit desde NetBeans

--- src/tarea6ed/Tarea6EDClase.java
+++ src/tarea6ed/Tarea6EDClase.java
@@ -5,7 +5,7 @@ public class Tarea6EDClase {
     private static final int MINIMO_PARA_REBAJA = 3;
     private static final double PRECIO_ESTANDAR = 0.95;
     private static final double PRECIO_REDUCIDO = 0.8;
-    private static final int DESCUENTO_EXTRA = 5;
+    private static final int DESCUENTO_EXTRA = 5;  
     
     public void aplicarDescuento(double precioProducto, int numProductos){
     double total;
