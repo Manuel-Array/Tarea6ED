9f8a49ce23f6ef63a455b1ebd1a1bc94becbf1e7
Arrabito <manuel.arra@outlook.com>
2025-05-18 20:03:03 +0200

    Commit desde NetBeans

--- src/tarea6ed/Tarea6EDClase.java
+++ src/tarea6ed/Tarea6EDClase.java
@@ -1,13 +1,20 @@
 package tarea6ed;
 
-    // holi, voy a hacer un commit con Netbeans
+/**
+ * Clase que aplica descuentos a productos según la cantidad comprada.
+ */
 public class Tarea6EDClase {
     
     private static final int MINIMO_PARA_REBAJA = 3;
     private static final double PRECIO_ESTANDAR = 0.95;
     private static final double PRECIO_REDUCIDO = 0.8;
     private static final int DESCUENTO_EXTRA = 5;
-    
+       
+    /**
+     * Aplica un descuento al precio del producto dependiendo de la cantidad.
+     * @param precioProducto Precio unitario original del producto
+     * @param numProductos Número total de productos comprados
+     */
     public void aplicarDescuento(double precioProducto, int numProductos){
     double total;
     if(numProductos>MINIMO_PARA_REBAJA)
@@ -22,6 +29,10 @@ public class Tarea6EDClase {
 
     }
 
+    /**
+     * Imprime el total a pagar en pantalla.
+     * @param total Cantidad final tras aplicar el descuento
+     */
     private void imprimirTotal(double total) {
         System.out.println("El total a pagar es:"+total);
         System.out.println("Enviado");
